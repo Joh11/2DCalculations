1065cbc9691c35b4f8670190276b367e8e5be3e8
Johan Félisaz <johan.felisaz@protonmail.com>
2020-11-26 13:12:31 +0100

    supercell found

--- generate_hrdat.py
+++ generate_hrdat.py
@@ -10,19 +10,17 @@ from sheet import Sheet
 # Input parameters
 # -----------------------------------------------------------------------------
 
-supercell_lattice = ...
 m = 7
-theta = np.arccos((3 * m**2 + 3 * m + 1/2) / (3 * m**2 + 3 * m + 1))
-
 
 # -----------------------------------------------------------------------------
 # Beginning of the script
 # -----------------------------------------------------------------------------
 
 # Generate the heterostructure
-N = 15
+N = 40 # TODO choose a big enough N
 
 a = 2.46
+interlayer_dist = 3.35
 a1 = np.array([a, 0, 0])
 a2 = np.array([a/2, sqrt(3)/2 * a, 0])
 a3 = np.array([0, 0, 1])
@@ -33,16 +31,19 @@ sites_A = np.array([0   * a1 + 0   * a2,
 sites_B = np.array([1/3 * a1 + 1/3 * a2,
                     2/3 * a1 + 2/3 * a2])
 
+theta = np.arccos((3 * m**2 + 3 * m + 1/2) / (3 * m**2 + 3 * m + 1))
+natoms_per_layer = 2 * (3 * m**2 + 3 * m + 1)
+natoms = 2 * natoms_per_layer
+
 s1 = Sheet([a1,a2,a3], ['C','C'], sites_A, [0], [-N,-N], [N,N], 'graphene')
 s2 = Sheet([a1,a2,a3], ['C','C'], sites_B, [0], [-N,-N], [N,N], 'graphene')
 
-h = Heterostructure([s1, s2], [0, -theta], [0, 3.35])
+h = Heterostructure([s1, s2], [0, theta], [0, interlayer_dist])
 
-# Generate the TB hoppings
-# /!\ Special case: format: [[intra-bottom, inter-bottom-top], [intra-top, intra-top-bottom]]
-xs = h.totalHamiltonian()
-intra_bottom, inter_bottom_top = xs[0]
-intra_top = xs[1][0]
+# Superlattice
+t1 = -(m+1) * a1 + (2*m+1) * a2
+t2 = (2*m+1) * a1 - m * a2
+t3 = interlayer_dist + 20 * np.array([0, 0, 1])
 
 # Find the position of each one of these indices
 def get_positions(h, s):
@@ -50,26 +51,43 @@ def get_positions(h, s):
     grid_indices = [h.sheets[s].indexToGrid(k) for k in range(max_index)]
     return np.array([h.posAtomGrid(grid_index, s) for grid_index in grid_indices])
 
+def indices_in_supercell(h, t1, t2, t3, origin=[0, 0, 0]):
+    """Returns a list of all (global) indices inside the given supercell. 
+    
+    Arguments: 
+    h          -- Heterostructure object
+    t1, t2, t3 -- supercell lattice vectors
+    origin     -- position of the supercell
+    """
+    origin = np.array(origin)
+    idcs = np.arange(h.max_index) # shape: (k,)
+    coords = np.concatenate([get_positions(h, s) for s in range(h.max_sheets)]) # shape: (k,3)
+    # translate
+    coords = coords - origin
+    # convert to frac coords
+    mat = np.linalg.inv(np.column_stack([t1, t2, t3])) # shape: (3,3)
+    # for broadcasting reasons do it in a transposed way
+    frac_coords = coords @ mat.transpose()
+
+    mask = ((frac_coords >= 0) * (frac_coords <= 1)).all(axis=1)
+    return idcs[mask]
+
+# shift it so that the center of the two sheets is at 1/3 t3
+idcs = indices_in_supercell(h, t1, t2, t3, origin=[0, 0, interlayer_dist/2 - t3[2]/2])
+
+print(len(idcs))
+assert(len(idcs) == natoms)
+
+# Generate the TB hoppings
+# /!\ Special case: format: [[intra-bottom, inter-bottom-top], [intra-top, intra-top-bottom]]
+xs = h.totalHamiltonian()
+intra_bottom, inter_bottom_top = xs[0]
+intra_top = xs[1][0]
+
 bottom_coords = get_positions(h, s=0)
 top_coords = get_positions(h, s=1)
 
-# Match these positions to ones of the reference POSCAR
-struct = mg.Structure.from_file('m7.vasp')
 
-# Shift the sites s.t. bottom -> bottom of the struct
-bottom_z_struct = np.min(struct.cart_coords[:, 2])
-bottom_coords[:, 2] = bottom_coords[:, 2] + bottom_z_struct
-top_coords[:, 2] = top_coords[:, 2] + bottom_z_struct
 
-def hetstruct_to_poscar(h, filename='scarr.vasp'):
-    coords = [get_positions(h, s) for s in range(h.max_sheets)]
-    coords = np.concatenate(coords, axis=0)
-    nsites = len(coords)
-    s = mg.Structure(np.array([[100, 0, 0],
-                               [0, 100, 0],
-                               [0, 0, 20]]),
-                     nsites * ['C'],
-                     coords + np.array([0, 0, 10 - 3.335 / 2]),# + np.array([50, 50, 2.5]),
-                     coords_are_cartesian=True)
-    Poscar(s).write_file(filename)
-hetstruct_to_poscar(h)
+# Find all the indices that are used
+
